ab20896f02c1f0146ef39b4673c59ab474098e55
David Nguyen <hpcsc@users.noreply.github.com>
2019-08-07 11:14:31 +1000

    initial commit

---
+++ run-local-dynamodb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CONTAINER_NAME=local-dynamodb
+
+[[ $(docker ps -a --filter "name=^/${CONTAINER_NAME}$" --format '{{.Names}}') == ${CONTAINER_NAME} ]] &&
+docker start ${CONTAINER_NAME} ||
+docker run -d --name ${CONTAINER_NAME} -p 8000:8000 amazon/dynamodb-local:1.11.477
